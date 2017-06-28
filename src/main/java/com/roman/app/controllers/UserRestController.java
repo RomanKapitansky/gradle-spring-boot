9b744401ab5e947300b1b028bdf8c4b149fafabe
blinker2d@gmail.com <jin9898>
2017-06-28 13:51:18 +0300

    added parameters to enable h2 console

--- src/main/java/com/roman/app/controllers/UserRestController.java
+++ src/main/java/com/roman/app/controllers/UserRestController.java
@@ -4,10 +4,14 @@ import com.roman.app.exception.NoSuchUserException;
 import com.roman.app.model.User;
 import com.roman.app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -17,9 +21,17 @@ public class UserRestController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/findUserById")
-    public User findUserById(@RequestParam(value="id") long id) {
-        return userRepository.findById(id);
+    @GetMapping(value = "/findUserById", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> findUserById(@RequestParam(value="id") long id) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            User user = userRepository.findById(id);
+            response.put("status", "success");
+            response.put("userData", user.toString());
+        } catch (Exception e) {
+            response.put("status", "user has not been found");
+        }
+        return response;
     }
 
     @GetMapping("/updateUserById")
